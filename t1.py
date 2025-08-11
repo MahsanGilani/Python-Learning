d2c397a97f74df6537f4bf75d361b25b068eda9e
Mahsan-Gilani <M.gilani@behsacorp.com>
2025-08-11 14:04:05 +0330

    new files added

---
+++ t1.py
@@ -0,0 +1,65 @@
+# x = 4
+# print(x:=2)
+
+# ************************************
+
+# x , y = 2 , 3
+# x , y = y , x
+# print(x , y)
+
+# ************************************
+
+# x , y = 2 , 3
+# print(x * y)
+
+# ************************************
+
+# x_santi = float(input("Write your temperature in centigrade: "))
+# x_faren = (x_santi * 9.5) + 32
+# print("Your temperature in fahrenheit is:",x_faren)
+
+# ************************************
+# import math
+
+# radius = int(input("Circle Radius: "))
+# mohit = radius * 2 * math.pi
+# masahat = math.pi * radius ** 2
+# print("masahat:", masahat)
+# print("mohit:", mohit)
+
+# ************************************
+# x , y = 20 , 10
+# # print(x + y)
+# # print(x - y)
+# # print(x * y)
+# # print(x / y)
+# # print(x % y)
+# # print(x // y)
+# # print(x ** y)
+# print((x + y),(x - y),(x * y),(x / y),(x % y),(x // y),(x ** y),end=", ", sep=", ")
+
+# ************************************
+
+# x , y , z = 1 , 2 , 3
+# print((x + y + z) / 3)
+
+# ************************************
+
+# ertefa , ghaede = 3 , 6
+# masahat = ertefa * ghaede / 2
+# print(masahat)
+
+# ************************************
+
+# my_bithday = 1376
+# my_bithday += 100
+# print(my_bithday)
+
+# ************************************
+
+# num1 = 42
+# num2 = 13
+# result = num1 * num2
+# print(result)
+
+# ************************************
