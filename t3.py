f1e2966013a578005629a6fc0bc8355a941007a5
Mahsan-Gilani <M.gilani@behsacorp.com>
2025-08-12 11:33:08 +0330

    new solution for joda kardan adad added

--- t3.py
+++ t3.py
@@ -1,6 +1,18 @@
-x = int(input("Enter a number: "))
-y = x // 100
-t = (x % 100) // 10
-z = x % 10
+# x = int(input("Enter a number: "))
+# y = x // 100
+# t = (x % 100) // 10
+# z = x % 10
 
-print(x , y , t , z, sep='\n')
+# print(x , y , t , z, sep='\n')
+
+# *******************************************************************
+# مثال بالارو بصورت زیر هم میشه حلش کرد:
+x = int(input("Enter a number: "))
+temp = x % 10
+print(temp)
+x = x // 10
+temp = x % 10
+print(temp)
+x = x // 10
+temp = x % 10
+print(temp)
